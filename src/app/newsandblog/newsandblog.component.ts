1f1176cf799af9a939ff9c61940eca4f753dbcef
unknown <jhanvisdesai@gmail.com>
2021-07-04 16:59:33 +0530

    News and Blog

--- src/app/newsandblog/newsandblog.component.ts
+++ src/app/newsandblog/newsandblog.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsandblog.component.scss']
 })
 export class NewsandblogComponent implements OnInit {
+  Newsdata:any;
+  now:any; 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.now = new Date().toJSON("yyyy/MM/dd HH:mm");
   }
 
+  ngOnInit(): void {
+    this.getnewsdata();
+  }
+  getnewsdata(){
+    //console.log("hello!");
+    fetch('https://hn.algolia.com/api/v1/search_by_date?query=menstrual&tags=story')
+    .then(response=>response.json())
+    .then(data=>{this.setNewsData(data);})
+  }
+  setNewsData(data:any){
+     
+    this.Newsdata = data; 
+   // console.log(this.now); 
+   // console.log(data.hits[0].created_at);
+  }
 }
