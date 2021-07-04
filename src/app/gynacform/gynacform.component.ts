3d7e69b1b2de4cdd8e9ac547a3d465868a69011f
Vaishnavi <vaishnavisdesai@gmail.com>
2021-07-04 21:24:43 +0530

    ...

---
+++ src/app/gynacform/gynacform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gynacform',
+  templateUrl: './gynacform.component.html',
+  styleUrls: ['./gynacform.component.scss']
+})
+export class GynacformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
